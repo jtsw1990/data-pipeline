7d4a84aa58db33ceabeb160fc2a5a6259fef9d14
jtsw1990 <jtsw1990@gmail.com>
2023-11-26 13:20:37 +1100

    feat: updated image gen to leonardo

--- src/handler_image_gen.py
+++ src/handler_image_gen.py
@@ -6,6 +6,7 @@ import openai
 import os
 import requests
 import base64
+import time
 
 
 def generate_image(event, context) -> None:
@@ -31,20 +32,34 @@ def generate_image(event, context) -> None:
 
     # GPT prompt
     openai.api_key = os.environ['openai_key']
+    leonardo_api_key = os.environ['leonardo_api_key']
 
     prompt_template = '''
-    Use the one main object or topic in the following news snippet and generate a creative DALL-E prompt using a 3d style in less than 30 words:
-    Bangladesh's worst ever dengue outbreak a 'canary in the coal mine' for climate crisis, WHO expert warns
+    Using the following snippet encased in ``, generate an image prompt of an imaginery human superhero or superheroine that is relevant to the snippet, be creating and detailed in the description of this character.
+    Also come up with a creative backstory in around 50 words on this hero's origin story and his/her main superpower, and include some elements of the original snippet in the backstory.
+    
+    `Faces of hope: The freed hostages of Gaza`
     '''  # noqa: E501
 
-    response_template = '''Produce a 3D artwork featuring the 'canary in the coal mine' symbolism for the climate crisis, with a spotlight on the dengue outbreak's significance.'''  # noqa: E501
+    response_template = '''
+    Imaginary Superhero: Lumina Salvator
+
+    Description:
+    Lumina Salvator, draped in a radiant cloak, emanates a warm glow reflecting the faces of the freed hostages. Their eyes shine with compassion, symbolizing the transformative power of hope emerging from the shadows.
+
+    Backstory:
+    Sarah Al-Hassan, a photojournalist, harnessed the energy of hope captured in her lens. Empowered, she became Lumina Salvator, using the light of captured moments to inspire healing and unity.
+
+    Main Superpower:
+    Photonic Healing - Lumina Salvator can harness and project healing energy through captured images, bringing solace and empowerment to those who have endured trauma, embodying the resilience of the freed hostages in Gaza.
+    '''  # noqa: E501
 
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {
                 'role': 'system',
-                'content': 'You are a professional graphic designer'
+                'content': 'You are a creative character designer'
             },
             {
                 'role': 'user',
@@ -56,7 +71,7 @@ def generate_image(event, context) -> None:
             },
             {
                 'role': 'user',
-                'content': f'Generate another DALL-E prompt for the following snippet: ```{initial_prompt}```'  # noqa: E501
+                'content': f'Do the same for the following snippet: `{initial_prompt}`'  # noqa: E501
             }
         ]
     )
@@ -65,16 +80,52 @@ def generate_image(event, context) -> None:
 
     features['img_prompt'] = image_prompt
 
-    # DALL-E Prompt
-    response = openai.Image.create(
-        prompt=image_prompt,
-        n=1,
-        size="512x512",
+    # Leonardo API
+
+    url = "https://cloud.leonardo.ai/api/rest/v1/generations"
+
+    payload = {
+        "height": 512,
+        "modelId": "1e60896f-3c26-4296-8ecc-53e2afecc132",
+        "prompt": image_prompt,
+        "width": 512,
+        "alchemy": True,
+        "highResolution": True,
+        "nsfw": True,
+        "num_images": 1,
+        "photoReal": False,
+        "presetStyle": "CINEMATIC",
+        "expandedDomain": True
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authorization": f"Bearer {leonardo_api_key}"
+    }
+    response = requests.post(url, json=payload, headers=headers)
+
+    for k in range(25):
+        print(f'Sleeping...{k}')
+        time.sleep(1)
+
+    gen_id = response.json()['sdGenerationJob']['generationId']
+
+    get_gen_url = f"https://cloud.leonardo.ai/api/rest/v1/generations/{gen_id}"
+
+    headers = {
+        "accept": "application/json",
+        "authorization": f"Bearer {leonardo_api_key}"
+    }
+
+    response = requests.get(get_gen_url, headers=headers)
+
+    img_url = (
+        response.json()
+        ['generations_by_pk']
+        ['generated_images'][0]['url']
     )
 
-    img_url = response["data"][0]["url"]
     img_bytes = requests.get(img_url).content
-
     img_byte_str = base64.b64encode(img_bytes).decode('utf-8')
 
     features['img_url'] = img_url
